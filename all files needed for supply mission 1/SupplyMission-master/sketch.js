32dfe17ea704d5d5bb7d761cad5887f1a740841d
shreyansh110 <69755759+shreyansh110@users.noreply.github.com>
2020-09-07 13:28:09 +0530

    Add files via upload

--- all files needed for supply mission 1/SupplyMission-master/sketch.js
+++ all files needed for supply mission 1/SupplyMission-master/sketch.js
@@ -31,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -50,15 +50,15 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
- 
+ keyPressed();
   drawSprites();
 
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-isStatic=false
-restition=1.0;
+
+	Matter.Body.setStatic(packageBody,false);
   }
 }
 
